facc981f082d57d52543011a9a1c510ec63bc81b
Ben Lambert <benlambert43@gmail.com>
2025-10-06 17:57:39 -0600

    add navbar items

--- home-web-ui/app/components/Navbar.tsx
+++ home-web-ui/app/components/Navbar.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Navbar = () => {
-  return <div className="flex justify-center items-center">Navbar</div>;
+  return (
+    <div className="flex justify-left items-center gap-4 bg-slate-600 min-w-full rounded-xl py-4 px-6">
+      <Link href="/">benlambert.tech</Link>
+      <Link href="/blog">blog</Link>
+      <Link href="/projects">projects</Link>
+      <Link href="/about">about</Link>
+    </div>
+  );
 };
 
 export default Navbar;
