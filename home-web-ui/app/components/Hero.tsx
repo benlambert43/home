e4134855ac8a402ecc00b6b0604d904fb5b62f42
Ben Lambert <benlambert43@gmail.com>
2025-10-07 14:28:04 -0600

    done with hero for now!

--- home-web-ui/app/components/Hero.tsx
+++ home-web-ui/app/components/Hero.tsx
@@ -1,9 +1,39 @@
 import Mountains from "../ui/Mountains";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div>Test</div>
+    <div className="flex flex-col items-center justify-center pt-8">
+      <div className="flex min-w-full items-center justify-end">
+        <div
+          className="flex min-w-1/2 flex-wrap-reverse items-center
+            justify-center gap-y-2"
+        >
+          <div className="px-4">
+            <h1>Hi there!</h1>
+            <h2>My name is Ben.</h2>
+            <h2>I am:</h2>
+
+            <ul className="list-inside list-disc">
+              <li>a software developer</li>
+              <li>a novice wildlife photographer</li>
+              <li>living in Denver, CO</li>
+            </ul>
+          </div>
+          <div
+            className="mx-4 max-h-40 max-w-40 overflow-clip rounded-tl-4xl
+              rounded-tr-4xl rounded-br-4xl rounded-bl-xl"
+          >
+            <Image
+              priority
+              src="/selfie.png"
+              width={500}
+              height={500}
+              alt="a selfie of Ben"
+            />
+          </div>
+        </div>
+      </div>
       <div className="max-w-full overflow-clip">
         <Mountains />
       </div>
--- home-web-ui/app/components/Navbar.tsx
+++ home-web-ui/app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div
-      className="justify-left flex min-w-full items-center gap-4 rounded-xl
-        bg-slate-600 px-6 py-4"
+      className="justify-left flex min-w-full flex-wrap items-center gap-4
+        rounded-xl bg-slate-600 px-6 py-4"
     >
       <Link href="/">benlambert.tech</Link>
       <Link href="/blog">blog</Link>
