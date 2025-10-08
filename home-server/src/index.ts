5f92841b423f7a7a73bfb0337717efd86ace93fa
Ben Lambert <benlambert43@gmail.com>
2025-10-08 13:20:21 -0600

    setup backend node server

---
+++ home-server/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+configDotenv();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const API_PORT = process.env.API_PORT;
+
+mongoose
+  .set("strictQuery", false)
+  .connect(process.env.MONGO_URI_ENV || "", {})
+  .then(() => {
+    console.log("MongoDB connected.");
+  })
+  .catch((err) => console.log(err))
+  .finally(() => {});
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Welcome to home-server" });
+});
+
+app.listen(API_PORT, () => {
+  console.log(`home-server is running on port ${API_PORT}`);
+});
