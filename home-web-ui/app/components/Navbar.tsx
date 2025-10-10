ac86ae73d4065de2f19cdf2e938d73ab6b1eab9b
Ben Lambert <benlambert43@gmail.com>
2025-10-10 16:51:20 -0600

    set up left and right parts of the navbar

--- home-web-ui/app/components/Navbar.tsx
+++ home-web-ui/app/components/Navbar.tsx
@@ -1,15 +1,28 @@
+import { Cog6ToothIcon } from "@heroicons/react/16/solid";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div
-      className="justify-left flex min-w-full flex-wrap items-center gap-4
-        rounded-xl bg-slate-600 px-6 py-4"
+      className="flex min-w-full flex-row flex-wrap-reverse items-center
+        justify-between gap-x-8 gap-y-6 rounded-xl bg-slate-600 px-6 py-4"
     >
-      <Link href="/">benlambert.tech</Link>
-      <Link href="/blog">blog</Link>
-      <Link href="/projects">projects</Link>
-      <Link href="/about">about</Link>
+      <div className="flex flex-4 flex-wrap items-center justify-start gap-4">
+        <Link href="/">benlambert.tech</Link>
+        <Link href="/blog">blog</Link>
+        <Link href="/projects">projects</Link>
+        <Link href="/about">about</Link>
+      </div>
+      <div className="flex flex-1 items-center gap-4 sm:justify-end">
+        <div>
+          <Link href="/">
+            <Cog6ToothIcon className="size-6" />
+          </Link>
+        </div>
+        <div className="min-w-12">
+          <Link href="/">sign in</Link>
+        </div>
+      </div>
     </div>
   );
 };
