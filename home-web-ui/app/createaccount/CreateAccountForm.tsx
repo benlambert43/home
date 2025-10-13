c87abbdd6f1e98b3439824568c5b36762ce85299
Ben Lambert <benlambert43@gmail.com>
2025-10-13 16:42:36 -0600

    add create account page

--- home-web-ui/app/createaccount/CreateAccountForm.tsx
+++ home-web-ui/app/createaccount/CreateAccountForm.tsx
@@ -3,15 +3,33 @@ import { createAccount } from "@/app/actions/auth";
 export const CreateAccountForm = () => {
   return (
     <form action={createAccount} className="flex flex-col gap-6">
-      <div className="flex flex-col items-start justify-center gap-2">
-        <label htmlFor="name">Name</label>
-        <input
-          className="w-full max-w-160 rounded-xl px-4 py-2 outline-1
-            outline-slate-400 focus:outline-slate-50"
-          id="name"
-          name="name"
-          placeholder="Name"
-        />
+      <div className="flex flex-row flex-wrap items-center justify-start gap-4">
+        <div
+          className="flex w-full max-w-78 flex-col items-start justify-center
+            gap-2"
+        >
+          <label htmlFor="firstname">First Name</label>
+          <input
+            className="w-full max-w-78 rounded-xl px-4 py-2 outline-1
+              outline-slate-400 focus:outline-slate-50"
+            id="firstname"
+            name="firstname"
+            placeholder="First Name"
+          />
+        </div>
+        <div
+          className="flex w-full max-w-78 flex-col items-start justify-center
+            gap-2"
+        >
+          <label htmlFor="lastname">Last Name</label>
+          <input
+            className="w-full max-w-78 rounded-xl px-4 py-2 outline-1
+              outline-slate-400 focus:outline-slate-50"
+            id="lastname"
+            name="lastname"
+            placeholder="Last Name"
+          />
+        </div>
       </div>
       <div className="flex flex-col items-start justify-center gap-2">
         <label htmlFor="username">Display Name</label>
