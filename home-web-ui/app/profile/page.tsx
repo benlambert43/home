ced60994fd184047eca4aaa515689ca4de6affba
Ben Lambert <benlambert43@gmail.com>
2025-10-21 17:11:17 -0600

    if there is no user cookie present and the user is trying to access their profile then redirect to the sign in page

--- home-web-ui/app/profile/page.tsx
+++ home-web-ui/app/profile/page.tsx
@@ -1,5 +1,20 @@
-const Profile = () => {
-  return <div>Profile</div>;
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { removeSession } from "@/app/actions/session";
+
+const Profile = async () => {
+  const cookieStore = await cookies();
+  const userCookie = cookieStore.get("user");
+  if (typeof userCookie?.value !== "string") redirect("/signin");
+
+  return (
+    <div className="p-5">
+      <div>Profile</div>
+      <div>
+        <button onClick={removeSession}>log out</button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
