64419073c5b19f329a29e2054ee64a0ceb0283c7
Ben Lambert <benlambert43@gmail.com>
2025-11-07 17:25:18 -0600

    make settings page only accessible to logged in users

--- home-web-ui/app/components/Navbar.tsx
+++ home-web-ui/app/components/Navbar.tsx
@@ -1,10 +1,42 @@
-import { Cog6ToothIcon } from "@heroicons/react/16/solid";
+import { Cog6ToothIcon, UserCircleIcon } from "@heroicons/react/16/solid";
 import Link from "next/link";
+import { cookies } from "next/headers";
 
-const Navbar = () => {
+const SignIn = () => {
+  return (
+    <div className="min-w-12">
+      <Link href="/signin">sign in</Link>
+    </div>
+  );
+};
+
+const Profile = () => {
+  return (
+    <div>
+      <Link href="/profile">
+        <UserCircleIcon className="size-6" />
+      </Link>
+    </div>
+  );
+};
+
+const Settings = () => {
+  return (
+    <div>
+      <Link href="/settings">
+        <Cog6ToothIcon className="size-6" />
+      </Link>
+    </div>
+  );
+};
+
+const Navbar = async () => {
+  const cookieStore = await cookies();
+  const userCookie = cookieStore.get("user");
+
   return (
     <nav
-      className="flex min-w-full flex-row flex-wrap-reverse items-center
+      className="mx-2 flex flex-row flex-wrap-reverse items-center
         justify-between gap-x-8 gap-y-6 rounded-xl bg-slate-600 px-6 py-4"
     >
       <div className="flex flex-4 flex-wrap items-center justify-start gap-4">
@@ -14,14 +46,14 @@ const Navbar = () => {
         <Link href="/about">about</Link>
       </div>
       <div className="flex flex-1 items-center gap-4 sm:justify-end">
-        <div>
-          <Link href="/settings">
-            <Cog6ToothIcon className="size-6" />
-          </Link>
-        </div>
-        <div className="min-w-12">
-          <Link href="/signin">sign in</Link>
-        </div>
+        {userCookie ? (
+          <>
+            <Settings />
+            <Profile />
+          </>
+        ) : (
+          <SignIn />
+        )}
       </div>
     </nav>
   );
--- home-web-ui/app/settings/page.tsx
+++ home-web-ui/app/settings/page.tsx
@@ -1,4 +1,10 @@
-const Settings = () => {
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const Settings = async () => {
+  const cookieStore = await cookies();
+  const userCookie = cookieStore.get("user");
+  if (typeof userCookie?.value !== "string") redirect("/signin");
   return <div>Settings</div>;
 };
 
