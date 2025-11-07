7bf2e26aa7050e92470ff3d2b3870907a9b76430
Ben Lambert <benlambert43@gmail.com>
2025-11-06 22:30:39 -0600

    rename middleware to proxy

--- home-web-ui/proxy.ts
+++ home-web-ui/proxy.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function middleware(req: NextRequest) {
+export default async function proxy(req: NextRequest) {
   return NextResponse.next();
 }
