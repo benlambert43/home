ba2167836b943c82a56ec0f213071bafe57193f2
Ben Lambert <benlambert43@gmail.com>
2025-12-26 10:03:25 -0700

    if the user isn't signed in, redirect them to the sign in page

--- home-web-ui/app/profile/accountManagement/requestNewEmailVerificationLink/page.tsx
+++ home-web-ui/app/profile/accountManagement/requestNewEmailVerificationLink/page.tsx
@@ -1,6 +1,12 @@
 import { RequestNewEmailVerificationLinkForm } from "@/app/profile/accountManagement/requestNewEmailVerificationLink/RequestNewEmailVerificationLinkForm";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const RequestNewEmailVerificationLink = async () => {
+  const cookieStore = await cookies();
+  const userCookie = cookieStore.get("user");
+  if (typeof userCookie?.value !== "string") redirect("/signin");
+
   return (
     <div className="mx-4 py-8">
       <RequestNewEmailVerificationLinkForm />
