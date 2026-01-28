23a0fd686c9890df6e4101c04433f31983849ebc
Ben Lambert <benlambert43@gmail.com>
2026-01-28 09:32:21 -0700

    parse the user cookie as UserCookie after confirming it exists

--- home-web-ui/app/profile/page.tsx
+++ home-web-ui/app/profile/page.tsx
@@ -1,16 +1,31 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { removeSession } from "@/app/actions/session";
+import { UserCookie } from "@/app/types/types";
 
 const Profile = async () => {
   const cookieStore = await cookies();
-  const userCookie = cookieStore.get("user");
-  if (typeof userCookie?.value !== "string") redirect("/signin");
+  const maybeUserCookie = cookieStore.get("user");
+  if (typeof maybeUserCookie?.value !== "string") redirect("/signin");
+  const userCookie = JSON.parse(maybeUserCookie.value) as UserCookie;
 
   return (
     <div className="p-5">
-      <div>{userCookie.value}</div>
       <div>Profile</div>
+      <div>{userCookie._id.toString()}</div>
+      <div>{userCookie.confirmedEmail.toString()}</div>
+      <div>{userCookie.createdDate.toString()}</div>
+      <div>{userCookie.email.toString()}</div>
+      <div>{userCookie.expiresAt.toString()}</div>
+      <div>{userCookie.firstname.toString()}</div>
+      <div>{userCookie.issuedAt.toString()}</div>
+      <div>{userCookie.lastname.toString()}</div>
+      <div>{userCookie.loginAt.toString()}</div>
+      <div>{userCookie.modifiedDate.toString()}</div>
+      <div>{userCookie.role.toString()}</div>
+      <div>{userCookie.userBanned.toString()}</div>
+      <div>{userCookie.username.toString()}</div>
+
       <div>
         <button onClick={removeSession}>log out</button>
       </div>
