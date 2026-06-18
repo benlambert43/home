32748360b1cdb490f6b95050ee4d93cab165d845
Ben Lambert <benlambert43@gmail.com>
2026-06-18 17:08:49 -0600

    Update ProfileBanner.tsx

--- home-web-ui/app/profile/ProfileBanner.tsx
+++ home-web-ui/app/profile/ProfileBanner.tsx
@@ -1,7 +1,16 @@
 import { UserCookie } from "@/app/types/types";
 
 const ProfileBanner = (props: { userCookie: UserCookie }) => {
-  return <></>;
+  return (
+    <>
+      {props.userCookie.confirmedEmail === false && (
+        <div className="max-w-80 rounded-md border-2 p-2">
+          Email is not yet confirmed. Please check your inbox or resend the
+          verification email.
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ProfileBanner;
