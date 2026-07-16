9aef41783e6508aa43ade84147bfc92d1abe24bb
Ben Lambert <benlambert43@gmail.com>
2026-07-16 13:33:37 -1000

    Request a new link added to profile page

--- home-web-ui/app/profile/ProfileBanner.tsx
+++ home-web-ui/app/profile/ProfileBanner.tsx
@@ -1,12 +1,28 @@
 import { UserCookie } from "@/app/types/types";
+import Button from "@/app/ui/Button";
+import Link from "next/link";
 
 const ProfileBanner = (props: { userCookie: UserCookie }) => {
   return (
     <>
       {props.userCookie.confirmedEmail === false && (
-        <div className="max-w-80 rounded-md border-2 p-2">
-          Email is not yet confirmed. Please check your inbox or resend the
-          verification email.
+        <div className="max-w-80 rounded-md border-2 p-4">
+          <div>
+            Email is not yet confirmed. Please check your inbox or resend the
+            verification email.
+          </div>
+          <div>
+            <div className="py-5">
+              <Link
+                href={
+                  "/profile/accountManagement/requestNewEmailVerificationLink"
+                }
+                className="font-semibold underline"
+              >
+                Request a New Link
+              </Link>
+            </div>
+          </div>
         </div>
       )}
     </>
