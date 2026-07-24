428665bf357236be74d3e1d0d739ac9c69a79b39
Ben Lambert <benlambert43@gmail.com>
2026-07-24 15:11:57 -0600

    add ability to use LinkButton

--- home-web-ui/app/profile/ProfileBanner.tsx
+++ home-web-ui/app/profile/ProfileBanner.tsx
@@ -1,4 +1,5 @@
 import { UserCookie } from "@/app/types/types";
+import Button from "@/app/ui/Button";
 import { ArrowRightIcon, EnvelopeIcon } from "@heroicons/react/16/solid";
 
 import Link from "next/link";
@@ -12,25 +13,30 @@ const ProfileBanner = (props: { userCookie: UserCookie }) => {
             Email is not yet confirmed. Please check your inbox or resend the
             verification email.
           </div>
-          <div>
-            <div className="flex flex-col gap-3 pt-6">
-              <Link
-                href={"http://google.com/gmail"}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+          <div className="flex flex-col gap-1 py-2">
+            <Button
+              type="link"
+              linkProps={{
+                href: "http://google.com/gmail",
+                target: "_blank",
+                rel: "noopener noreferrer",
+              }}
+              size="small"
+            >
+              <div className="flex flex-row gap-1">
                 <EnvelopeIcon className="size-6" />
                 Open Gmail
-              </Link>
-              <Link
-                href={
-                  "/profile/accountManagement/requestNewEmailVerificationLink"
-                }
-                className="underline"
-              >
-                Request a New Link
-              </Link>
-            </div>
+              </div>
+            </Button>
+
+            <Link
+              href={
+                "/profile/accountManagement/requestNewEmailVerificationLink"
+              }
+              className="underline"
+            >
+              Request a New Link
+            </Link>
           </div>
         </div>
       )}
--- home-web-ui/app/ui/Button.tsx
+++ home-web-ui/app/ui/Button.tsx
@@ -1,33 +1,66 @@
-import { MouseEventHandler, ReactNode } from "react";
+import { Url } from "next/dist/shared/lib/router/router";
+import Link from "next/link";
+import { HTMLAttributeAnchorTarget, MouseEventHandler, ReactNode } from "react";
+
+type LinkProps = {
+  href: Url;
+  target?: HTMLAttributeAnchorTarget | undefined;
+  rel?: string | undefined;
+};
 
 const Button = (props: {
   children: ReactNode;
-  type?: "button" | "reset" | "submit" | undefined;
+  type?: "button" | "reset" | "submit" | "link" | undefined;
+  linkProps?: LinkProps;
   size?: "large" | "medium" | "small";
   disabled?: boolean | undefined;
   onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
   className?: string | undefined;
 }) => {
-  const { children, type, size, disabled, onClick, className } = props;
+  const { children, type, linkProps, size, disabled, onClick, className } =
+    props;
 
-  return (
-    <button
-      type={type}
-      disabled={disabled}
-      onClick={onClick}
-      className={
-        className || size === "large"
-          ? `w-1/2 max-w-80 min-w-fit rounded-xl bg-slate-500 px-4 py-2
+  const evalClassName =
+    className || size === "large"
+      ? `w-1/2 max-w-80 min-w-fit rounded-xl bg-slate-500 px-4 py-2
             hover:cursor-pointer hover:bg-slate-400 focus:outline-slate-100`
-          : size === "medium"
-            ? ""
-            : `className="flex focus:outline-slate-100" gap-x-2 rounded-sm
-              bg-slate-500 px-2 py-1 hover:cursor-pointer hover:bg-slate-400`
-      }
-    >
-      {children}
-    </button>
-  );
+      : size === "medium"
+        ? ""
+        : size === "small"
+          ? `w-1/2 max-w-60 min-w-fit rounded-md bg-slate-500 px-1 py-0.5
+              hover:cursor-pointer hover:bg-slate-400 focus:outline-slate-100`
+          : "";
+
+  if (
+    type === "button" ||
+    type === "submit" ||
+    type === "reset" ||
+    type === undefined
+  ) {
+    return (
+      <button
+        type={type}
+        disabled={disabled}
+        onClick={onClick}
+        className={evalClassName}
+      >
+        {children}
+      </button>
+    );
+  } else if (type === "link" && linkProps?.href) {
+    return (
+      <Link
+        href={linkProps?.href}
+        target={linkProps?.target}
+        rel={linkProps.rel}
+        className={evalClassName}
+      >
+        {children}
+      </Link>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default Button;
