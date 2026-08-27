ba0c2627defae2ede7edd755388fbdbd5f43e41d
Ben Lambert <benlambert43@gmail.com>
2026-08-26 20:02:07 -0600

    don't delete email verification records for the user when deleting the user

--- home-server/src/api/v1/accountManagement/handlers/handleDeleteAccount.ts
+++ home-server/src/api/v1/accountManagement/handlers/handleDeleteAccount.ts
@@ -1,5 +1,4 @@
 import { DeleteAccountResponse, EncodedAccountJwt } from "@home/shared";
-import { EmailVerificationModel } from "../../model/emailVerificationModel";
 import { NotificationModel } from "../../model/notificationModel";
 import { UserModel } from "../../model/userModel";
 import { ApiMessage } from "../../http/messages";
@@ -16,7 +15,6 @@ export const handleDeleteAccount = async (
   }
 
   await NotificationModel.deleteMany({ recipientUserId: foundUser._id });
-  await EmailVerificationModel.deleteMany({ userId: foundUser._id });
   await UserModel.findByIdAndDelete(foundUser._id);
 
   return { error: false, message: ApiMessage.ACCOUNT_DELETED };
