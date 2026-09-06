b7bc0464efd52f96627641600e512ecf610dfaeb
Ben Lambert <benlambert43@gmail.com>
2026-09-05 18:29:17 -0600

    add optional canonical path for paginated urls

--- home-web-ui/app/lib/metadata.ts
+++ home-web-ui/app/lib/metadata.ts
@@ -4,11 +4,18 @@ const SITE_NAME = "ben lambert";
 const SITE_DESCRIPTION = "ben lamberts personal website 🧑‍💻";
 
 export const siteMetadata: Metadata = {
+  metadataBase: new URL(process.env.BASE_SITE_URL ?? "http://localhost:3000"),
   title: SITE_NAME,
   description: SITE_DESCRIPTION,
 };
 
-export const pageMetadata = (page: string): Metadata => ({
+export const pageMetadata = (
+  page: string,
+  canonicalPath?: string,
+): Metadata => ({
   title: `${SITE_NAME} - ${page}`,
   description: SITE_DESCRIPTION,
+  ...(canonicalPath === undefined
+    ? {}
+    : { alternates: { canonical: canonicalPath } }),
 });
