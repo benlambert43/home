172496b132ddf11252d534cc7b0b01d81c873bae
Ben Lambert <benlambert43@gmail.com>
2026-09-08 16:26:43 -0600

    remove prefetching for now

--- home-web-ui/next.config.ts
+++ home-web-ui/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   cacheComponents: true,
-  partialPrefetching: true,
 };
 
 export default nextConfig;
